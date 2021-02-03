917303238983dba9e9b432fb31f53501865c250e
ajay0patel <51459396+ajay0patel@users.noreply.github.com>
2021-02-03 22:16:08 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -11,16 +11,15 @@ int lcs(string s1,string s2,int m,int n,int memo[][maximum]){
         memo[m][n]=0;
     }
     
-    else
+    else{
     
     if(s1[m-1]==s2[n-1]){
         memo[m][n]=1+lcs(s1,s2,m-1,n-1,memo);
     }
-    
     else{
     memo[m][n]=max(lcs(s1,s2,m,n-1,memo),lcs(s1,s2,m-1,n,memo));
     }
-    
+    }
     return memo[m][n];
 }
 
